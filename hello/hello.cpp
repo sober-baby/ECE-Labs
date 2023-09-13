f105f74dca3fa93f467ffbda9cea018cc4d5ee1a
sober-baby <61091443+sober-baby@users.noreply.github.com>
2023-09-13 00:04:24 -0400

    asd

---
+++ hello/hello.cpp
@@ -0,0 +1,12 @@
+// hello.cpp -- "Hello, world" C++ program
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    cout << "Hello " << endl;
+    cout << "world!" << endl;
+    return (0);
+}
+
